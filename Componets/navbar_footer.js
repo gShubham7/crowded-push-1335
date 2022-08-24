881ec730b5a80d9b0575692f8c4f3fc26a97d836
subhamukherjee321 <subhamukherjee321@gmail.com>
2022-08-24 22:02:57 +0530

    day 2 project added

---
+++ Componets/navbar_footer.js
@@ -0,0 +1,115 @@
+const navbar = () => {
+
+      return `<div id="logo">
+            <a href="index.html">
+                  <img src="https://secureservercdn.net/45.40.145.201/79x.0e9.myftpupload.com/wp-content/uploads/2020/11/SparkAmerica2020-02-515x65.png"
+                        alt="">
+            </a>
+      </div>
+      <div id="category">
+            <div id="lists">
+                  <ul>
+                        <li>
+                              <a href="">
+                                    JOIN THE MOVEMENT
+                              </a>
+                        </li>
+                        <li>
+                              <a href="">
+                                    PARTNERS
+                              </a>
+                        </li>
+                        <li>
+                              <a href="">
+                                    CALENDER
+                              </a>
+                        </li>
+                        <li>
+                              <a href="">
+                                    THE APP
+                              </a>
+                        </li>
+                        <li>
+                              <a href="">
+                                    CONTACT US
+                              </a>
+                        </li>
+                        <li>
+                              <a href="">
+                                    REGISTER
+                              </a>
+                        </li>
+                  </ul>
+
+                  <ul>
+                        <li>
+                              <a href="">
+                                    <i class="fa-brands fa-square-facebook"></i>
+                              </a>
+                        </li>
+                        <li>
+                              <a href="">
+                                    <i class="fa-brands fa-instagram"></i>
+                              </a>
+                        </li>
+                  </ul>
+            </div>
+      </div>`
+
+}
+
+
+
+const footer = () =>{
+      return `<div>
+            <div>
+                  <h3>CONTACT US</h3>
+                  <ul>
+                        <li>
+                              <a href="">support@sparkamerica.com</a>
+                        </li>
+                        <li>
+                              (888) 330-6891
+                        </li>
+                        <li>
+                              SparkAmerica
+                        </li>
+                        <li>
+                              P.O. Box 123, Oakmont, PA 15139
+                        </li>
+                  </ul>
+            </div>
+
+            <div >
+                  <h3>CONTACT US</h3>
+                  <ul>
+                        <li>
+                              <a href="">JOIN THE MOVEMENT</a>
+                        </li>
+                        <li>
+                              <a href="">PARTNERS</a>
+                        </li>
+                        <li>
+                              <a href="">CALENDAR</a>
+                        </li>
+                        <li>
+                              <a href="">THE APP</a>
+                        </li>
+                        <li>
+                             <a href="">CONTACT US</a> 
+                        </li>
+                        <li>
+                              <a href="">REGISTER</a>
+                        </li>
+                  </ul>
+            </div>
+      </div>
+
+      <div id="copy_right">
+            ® 2020 PeopleOne Health. All Rights Reserved.
+      </div>`
+}
+
+
+
+export {navbar, footer};
